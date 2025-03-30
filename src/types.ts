a8c8c20cae1bb25f9b8c0871499e29edde6a4f7e
Eric Jones <etjones319@gmail.com>
2025-03-30 10:52:11 -0700

    setup app component with mock data

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export interface CrashData {
+  app_version: string;
+  crash_count: number[];
+  timestamp: number[];
+  device_type: string;
+}
+
+export interface ApiResponse {
+  end: string;
+  start: string;
+  step: number;
+  data: CrashData[];
+}
+
+export interface TransformedData {
+  name: string;
+  [key: string]: number | string;
+}
